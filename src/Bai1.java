0411190624516ea68dedbbb2f868cf0c6de01d0c
minhduz <minhducoverwatch@gmail.com>
2022-09-30 06:49:01 +0700

    first commit

---
+++ src/Bai1.java
@@ -0,0 +1,15 @@
+import rikkei.academy.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student(111,"khánh");
+        Student s2 = new Student(222,"khánh");
+        Student s3 = new Student(222,"khánh");
+
+        Student.change();
+        s1.display();
+        s2.display();
+        s3.display();
+
+    }
+}
